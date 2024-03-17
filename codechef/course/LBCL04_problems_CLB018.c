1ff420fd298403b68648662d3f8a999facf739e0
Asmaul360 <asmaulmallick360@gmail.com>
2024-03-17 23:49:51 +0530

    Division

---
+++ codechef/course/LBCL04_problems_CLB018.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	// Print the result of 9 - 2
+	int a=9;
+	int b=2;
+	int sub=a-b;
+	int x=30;
+	int y=5;
+	int div=x/y;
+	printf("%d\n",sub);
+	
+	// Print the result of 30 / 5
+	printf("%d\n",div);
+	
+}
