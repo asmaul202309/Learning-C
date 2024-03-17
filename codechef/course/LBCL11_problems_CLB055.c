fec43e1d5dbe2626d65fc92abd561f33f92725a2
Asmaul360 <asmaulmallick360@gmail.com>
2024-03-18 00:34:32 +0530

    Calculator

---
+++ codechef/course/LBCL11_problems_CLB055.c
@@ -0,0 +1,16 @@
+
+int main() {
+
+  int a, b;
+  
+  scanf("%d", & a);
+  scanf("%d", & b);
+   int sum=a+b;
+  int diff=a-b;
+  printf("Sum is: %d\n",sum);
+  printf("Difference is: %d\n",diff);
+  
+  
+  
+  
+}
