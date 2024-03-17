b9815abaef184ccd85a984994924708a45fc93d7
Asmaul360 <asmaulmallick360@gmail.com>
2024-03-18 00:07:43 +0530

    double datatype

--- codechef/course/LBCL05_problems_CLB026.c
+++ codechef/course/LBCL05_problems_CLB026.c
@@ -3,9 +3,9 @@
 int main() {
 
   double pi = 3.14;
-  double radius = 3.14;
+  double radius = 8.9;
   double area = pi * radius * radius;
 
-  printf("The Area of the given Circle is %d", area);
+  printf("The Area of the given Circle is %f", area);
   
 }
