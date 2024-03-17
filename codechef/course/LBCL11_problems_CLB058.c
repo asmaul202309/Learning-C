6f393495a86aafa470b6b9881e8b1b96dbdbe237
Asmaul360 <asmaulmallick360@gmail.com>
2024-03-18 00:39:15 +0530

    Hello user

---
+++ codechef/course/LBCL11_problems_CLB058.c
@@ -0,0 +1,13 @@
+   #include <stdio.h>
+
+int main() {
+ char x[10];
+    
+    // Take user input in variable x
+    scanf("%d",& x);
+    
+    // Print greeting
+    printf("Hello Chef", x);
+    
+    
+}
