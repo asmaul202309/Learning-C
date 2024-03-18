d841733585bb2b827a6722842ff8d0cdb26ce99d
Asmaul360 <asmaulmallick360@gmail.com>
2024-03-18 23:26:15 +0530

    number mirror

---
+++ codechef/practice/PCL01_problems_START01.c
@@ -0,0 +1,38 @@
+// Number Mirror
+// Write a program that takes a number 
+// �
+// N as the input, and prints it to the output.
+
+// Input Format
+// The only line of input contains a single integer.
+
+// Output Format
+// Output the answer in a single line.
+
+// Constraints
+// 0
+// ≤
+// �
+// ≤
+// 1
+// 0
+// 5
+// 0≤N≤10 
+// 5
+ 
+// Sample 1:
+// Input
+// Output
+// 123
+// 123
+
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int n;
+	scanf("%d",& n);
+	 printf("%d\n",n);
+
+}
+
