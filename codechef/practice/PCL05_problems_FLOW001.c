c697d1009ed09fa191fe83a9adae1c4e140ba091
Asmaul360 <asmaulmallick360@gmail.com>
2024-03-19 01:00:40 +0530

    add twoo numbers

---
+++ codechef/practice/PCL05_problems_FLOW001.c
@@ -0,0 +1,60 @@
+// Add Two Numbers
+// Your task is very simple: given two integers 
+// �
+// A and 
+// �
+// B, write a program to add these two numbers and output the sum.
+
+// Input Format
+// The first line contains an integer 
+// �
+// T, the total number of test cases.
+// Then follow 
+// �
+// T lines, each line contains two integers, 
+// �
+// A and 
+// �
+// B.
+// Output Format
+// For each test case, add 
+// �
+// A and 
+// �
+// B and display the sum in a new line.
+
+// Constraints
+// 1
+// ≤
+// �
+// ≤
+// 1000
+// 1≤T≤1000
+// 0
+// ≤
+// �
+// ,
+// �
+// ≤
+// 10000
+// 0≤A,B≤10000
+
+
+// #include <stdio.h>
+
+// int main() {
+//     int t;
+//     scanf("%d", &t);
+
+//     // Loop for each test case
+//     while (t--) {
+//         int a, b;
+
+//         scanf("%d %d", &a, &b);
+//           int sum = a + b;
+
+//         // Output the sum
+//         printf("%d\n", sum);
+//         // Your code for each test case goes here 
+//     }
+// }
